adc2c88b5bbd7e8a1cd0cded1a1ad8aa287873b3
Daichi Kudo <sakanactor624@gmail.com>
2020-05-23 09:46:32 +0900

    :sparkles: 本をレコメンドしてくれる機能を追加

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,46 @@ import { getDayFormat } from "./util";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare let global: any;
 
-const getPhraseFromSpreadSheet = (): string => {
+const getPhraseFromSpreadSheet = (): string | any => {
   const ss = SpreadsheetApp.openById(
     "10YAPVNZtbq8JVLtBX9VsAqmGXqfctZIyUjbcwRCHuXc"
   );
   const sheet = ss.getSheetByName("sheet1");
-  const values = sheet
-    .getRange("B:B")
-    .getValues()
-    .filter(String);
+  const values = sheet.getDataRange().getValues();
 
   const phraseArray = values.slice(1, values.length);
   const phraseSelector = Math.floor(Math.random() * phraseArray.length);
-  const text = phraseArray[phraseSelector].toString();
+  const text = phraseArray[phraseSelector];
   return text;
 };
 
+const getBookFromSpreadSheet = (): string[] => {
+  const ss = SpreadsheetApp.openById(
+    "10YAPVNZtbq8JVLtBX9VsAqmGXqfctZIyUjbcwRCHuXc"
+  );
+  const sheet = ss.getSheetByName("sheet2");
+  const values = sheet.getDataRange().getValues();
+  const books = values.slice(1, values.length);
+  const bookSelector = Math.floor(Math.random() * books.length);
+  const book = books[bookSelector];
+  return book;
+};
+
 global.record = (): void => {
-  const text = getPhraseFromSpreadSheet();
+  const phrase = getPhraseFromSpreadSheet();
+  const book = getBookFromSpreadSheet();
+  const text =
+    "今週のおすすめ本は「" +
+    book[0] +
+    "」です！" +
+    "\n" +
+    "著者: " +
+    book[1] +
+    "\n" +
+    "ポイント: " +
+    book[2] +
+    "\n\n" +
+    phrase;
   const payload = {
     text: text
   };
@@ -30,6 +52,6 @@ global.record = (): void => {
     payload: JSON.stringify(payload)
   };
   const url =
-    "https://hooks.slack.com/services/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";
+    "https://hooks.slack.com/services/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";
   UrlFetchApp.fetch(url, options);
 };
